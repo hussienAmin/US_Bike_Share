8588d5aae0a1d7a03f1ae7f3ab539a07db5be1de
Aussien A Amin <122715805+hussienAmin@users.noreply.github.com>
2024-08-24 16:52:46 +0300

    Update models.csgfg

--- models.cs
+++ models.cs
@@ -16,3 +16,46 @@ public ArWord ArWord{get;set;}
 public int EnId{get;set;}
 
 }
+partial class MyDbContext(string con) : DbContext
+{
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+            
+
+            
+            optionsBuilder.UseSqlite(con);
+            optionsBuilder.UseExceptionProcessor();
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("seketo");
+            
+
+        }
+
+
+
+        //               MANAGE MODULE
+
+
+        public DbSet<EnWord> EnWords { get; set; }
+        public DbSet<ArWord> ArWords { get; set; }
+        public DbSet<EnAr> EnArs { get; set; }
+        
+
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Device> devices { get; set; }
+
+        void managemodule(ModelBuilder modelBuilder)
+        {
+            //UNIQUE
+            modelBuilder.Entity<Appblob>().HasIndex(p => p.Name).IsUnique();
+            
+
+            //RELATION
+            
+            modelBuilder.Entity<User>().HasMany<Note>(g => g.Noteslist).WithOne(p => p.User).OnDelete(DeleteBehavior.Restrict);
+            
+
+        }
